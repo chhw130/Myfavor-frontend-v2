dfee58db5b551290a8572a7f8112a64795b0dc22
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2023-05-12 23:06:13 +0900

    update : instance axios

--- src/axios-settings/Axios.js
+++ src/axios-settings/Axios.js
@@ -31,4 +31,66 @@ export const specificIdolInform = (idolId) =>
   instance.get(`/idols/${idolId}/`).then((response) => response.data);
 
 export const specificIdolSchedule = (idolId) =>
-  instance.get(`idols/${idolId}/schedules`).then((response) => response.data);
+  instance.get(`/idols/${idolId}/schedules/`).then((response) => response.data);
+
+/**사진을 업로드 할 url 가져오는 함수. */
+export const getUploadUrl = async (img) => {
+  let resData = "";
+  await instance
+    .post(`/media/photos/get-url/`, img.file)
+    .then((data) => {
+      resData = uploadImg(data, img);
+      return resData;
+    })
+    .catch(() => {
+      resData = false;
+      return resData;
+    });
+
+  return resData;
+};
+
+/**받아온 url에 img를 넣어주기 */
+export const uploadImg = async (data, img) => {
+  let resData = "";
+  const form = new FormData();
+  form.append("file", img.file[0]);
+  await axios
+    .post(data.data.uploadURL, form, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      withCredentials: false,
+    })
+    .then((res) => {
+      resData = res.data.result;
+      return res.data.result;
+    });
+
+  return resData;
+};
+
+/**유저 이미지를 넣은 url post 하기 */
+export const postProfileImg = async (profileImg) => {
+  await instance
+    .put(`/users/mypage/`, profileImg, {
+      withCredentials: true,
+    })
+    .then((res) => res)
+    .catch((res) => res);
+};
+
+/**유저 일정 등록 */
+
+export const postUserCalendar = async (data) =>
+  await instance.post(`/users_calendar/`, data, {
+    withCredentials: true,
+  });
+
+/**유저 일정 수정 */
+
+export const putUserCalendar = async (data, schedulePk) => {
+  await instance.put(`/users_calendar/${schedulePk}/`, data, {
+    withCredentials: true,
+  });
+};
